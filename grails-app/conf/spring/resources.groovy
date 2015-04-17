7a5a89bbd96b9215ea5f90af1543b60787308f9d
Kazuki YAMAMOTO <yam.kazuki@gmail.com>
2015-04-17 14:57:18 +0900

    Custom Marshaller

--- grails-app/conf/spring/resources.groovy
+++ grails-app/conf/spring/resources.groovy
@@ -1,9 +1,3 @@
-import grails.rest.render.json.JsonRenderer
-import sample.Book
-
 // Place your Spring DSL code here
 beans = {
-    bookRenderer(JsonRenderer, Book) {
-        includes = ['title']
-    }
 }
--- grails-app/init/BootStrap.groovy
+++ grails-app/init/BootStrap.groovy
@@ -1,8 +1,16 @@
+import grails.converters.JSON
 import sample.Book
 
 class BootStrap {
 
     def init = { servletContext ->
+        JSON.registerObjectMarshaller(Book) { Book book, JSON converter ->
+            converter.build {
+                id book.id
+                title book.title
+            }
+        }
+
         10.times {
             new Book(title: "test book ${it}").save()
         }

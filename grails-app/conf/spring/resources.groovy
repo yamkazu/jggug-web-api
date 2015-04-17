43bbd970e6a5102937f524545a2f1d1bd81092bb
Kazuki YAMAMOTO <yam.kazuki@gmail.com>
2015-04-17 14:53:18 +0900

    Simple Renderer

--- grails-app/conf/spring/resources.groovy
+++ grails-app/conf/spring/resources.groovy
@@ -1,3 +1,9 @@
+import grails.rest.render.json.JsonRenderer
+import sample.Book
+
 // Place your Spring DSL code here
 beans = {
+    bookRenderer(JsonRenderer, Book) {
+        includes = ['title']
+    }
 }
--- grails-app/init/BootStrap.groovy
+++ grails-app/init/BootStrap.groovy
@@ -1,6 +1,11 @@
+import sample.Book
+
 class BootStrap {
 
     def init = { servletContext ->
+        10.times {
+            new Book(title: "test book ${it}").save()
+        }
     }
     def destroy = {
     }
